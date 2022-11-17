424577d0e3f3b907759f4018ddc2debdb610c88d
“yetish” <yetish.shivanna519@gmail.com>
2022-11-17 08:43:00 +0000

    changes by yetish

--- patternfinding.sh
+++ patternfinding.sh
@@ -4,7 +4,7 @@ read string
 grep -iRl "$string" * > outfile_pattern
 if [ $? -eq 0 ]
 then
-	echo "The below files contains the pattern $string"
+	echo "$string"
 	cat outfile_pattern
 else
 	echo "No file contains the pattern $string"
