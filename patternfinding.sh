5d6098c3ea8ea4c6ddffb8264ffa011fed5f9705
jnanaswaroop <jnanaswaroop@gmail.com>
2022-11-17 09:02:37 +0000

    Jnana commiting

--- patternfinding.sh
+++ patternfinding.sh
@@ -4,10 +4,10 @@ read string
 grep -iRl "$string" * > outfile_pattern
 if [ $? -eq 0 ]
 then
-	echo "The below files contains the pattern $string Jnana swaroop k r"
+	echo "The  files contains the pattern $string Jnana swaroop k r"
 	cat outfile_pattern
 else
-	echo "No file contains the pattern $string Jnana_swaroop k r"
+	echo "No file contains the pattern $string"
 fi
 
 
