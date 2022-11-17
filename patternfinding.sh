55379e053859a4756a18ac310302892b452f7c51
jnanaswaroop <jnanaswaroop@gmail.com>
2022-11-17 08:54:17 +0000

    Jnana commiting

--- patternfinding.sh
+++ patternfinding.sh
@@ -7,5 +7,7 @@ then
 	echo "The below files contains the pattern $string Jnana swaroop k r"
 	cat outfile_pattern
 else
-	echo "No file contains the pattern $string Jnana swaroop k r"
+	echo "No file contains the pattern $string Jnana_swaroop k r"
 fi
+
+
